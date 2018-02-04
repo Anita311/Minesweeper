7d8d0f0baabfc88702892c090fdab52060a61f40
Sakshi Tiwari <tiwari.sakshi96.st@gmail.com>
2018-02-05 02:50:35 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,13 +2,14 @@
 // drive the complete code
 
 #include"myHeader.cpp"
-int main(int argc, char **argv)
+int main()
 {
 	int choice, mines, size, row, col, win = 0;
 	char realGrid[25][25], myGrid[25][25], flag;
 	cout << "\t\t\tWelcome to Minesweeper " << endl;
 	cout << " Instructions : " <<  endl << endl;
 	cout << "Enter your move in the format : 1 2" << endl;
+	cout << "To flag some location, simply hit f and enter it's location" << endl;
 	cout << endl;
 	cout << "Press 1 for Beginners level " << endl;
 	cout << "Press 2 for Medium level " << endl;
@@ -31,88 +32,75 @@ int main(int argc, char **argv)
 	{
 		size = 25;
 		mines = 99;
-	}
-		
-	int f = inputValid(choice);
-	test(argc, argv);
-	if(f)
-		{
-		generateMinesweeper(realGrid, myGrid, size, mines);
-		int m = mines;
-		printGrid(myGrid, size);
-		cout << endl;
-		
-		// Play until to win
-		while(!win){
-			cout << endl << "Enter the location you want to unlock : ";	
-			cin  >> row >> col ;
-			cout <<  endl;
-			if(checkValid(row, col, size))
-			{
-				win = minesweeperPlayer(myGrid, realGrid, row, col, size);
-				printGrid(myGrid, size);
-				int n = unknownsLeft(myGrid, size);
-				if(win == 1 || n == mines)
-				{
-					cout << "Yeah babe..!!, You did it";
-					win = 1; 
-				}
-			}
-			else
-				cout << "Enter valid location ";
-				
-			// Uncomment following lines to enable flag in the game
-				
-			/*cout << endl << "Do you want to flag a certain location : Y/N";
-			cin >> flag	;
-			if(toupper(flag) == 'Y'){
-				cout << endl << "Enter the location you want to flag : ";
-				cin >> row >> col ;
-				if(checkValid(row, col, size))
-					myGrid[row][col] = '#';
-				printGrid(myGrid, size);	
-				m-- ;
-				cout << endl << "  Mines Left : " << m;
-			}	*/
-			
-			}
-		}
-		else{
-			cout << "Enter valid entry" << endl;
-		}
 	}	
-		// Function to check unknown left
-		int unknownsLeft(char myGrid[][25], int size)
+	generateMinesweeper(realGrid, myGrid, size, mines);
+	int m = mines;
+	printGrid(myGrid, size);
+	cout << endl;
+	
+	// Play until to win
+	while(!win){
+		cout << endl << "Enter the location you want to unlock : ";	
+		row = flag;
+		cin  >> row >> col ;
+		cout <<  endl;
+		if(checkValid(row, col, size))
 		{
-			int c = 0;
-			for(int i = 0; i < size; i++)
+			win = minesweeperPlayer(myGrid, realGrid, row, col, size);
+			printGrid(myGrid, size);
+			int n = unknownsLeft(myGrid, size);
+			if(win == 1 && n == mines)
 			{
-				for(int j = 0; j < size; j++)
-				{
-					if(myGrid[i][j] == '-')
-						c++;
-				}
+				cout << "Yeah babe..!!, You did it";
+				win = 1; 
 			}
-			return c;
 		}
-		
-		// Function to check if the entered dimension is valid or not
-		int checkValid(int row, int col, int size)
+		else
+			cout << "Enter valid location ";
+		cout << endl << "Do you want to flag a certain location : Y/N";
+		cin >> flag	;
+		if(toupper(flag) == 'Y'){
+			cout << endl << "Enter the location you want to flag : ";
+			cin >> row >> col ;
+			if(checkValid(row, col, size))
+				myGrid[row][col] = '#';
+			printGrid(myGrid, size);	
+			m-- ;
+			cout << endl << "  Mines Left : " << m;
+		}		
+	}
+	return 0; 
+}	
+	// Function to check unknown left
+	int unknownsLeft(char myGrid[][25], int size)
+	{
+		int c = 0;
+		for(int i = 0; i < size; i++)
 		{
-			if(row < size && col <size)
-				return 1;
-			else
-				return 0;
+			for(int j = 0; j < size; j++)
+			{
+				if(myGrid[i][j] == '-')
+					c++;
+			}
 		}
-		
-		// Function to check for the presence for any mine
-		int checkMine(char realGrid[][25], int row, int col)
-		{
-			if(realGrid[row][col] == '*')
-				return 1;
-			else
-				return 0;
-		}
-		
-
+		return c;
+	}
+	
+	// Function to check if the entered dimension is valid or not
+	int checkValid(int row, int col, int size)
+	{
+		if(row < size && col <size)
+			return 1;
+		else
+			return 0;
+	}
+	
+	// Function to check for the presence for any mine
+	int checkMine(char realGrid[][25], int row, int col)
+	{
+		if(realGrid[row][col] == '*')
+			return 1;
+		else
+			return 0;
+	}
 
