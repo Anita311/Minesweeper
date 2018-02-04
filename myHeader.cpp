496e2ec3bb0369f56ec3f059c9665c948e8dd077
Sakshi Tiwari <tiwari.sakshi96.st@gmail.com>
2018-02-05 02:51:23 +0530

    Update myHeader.cpp

--- myHeader.cpp
+++ myHeader.cpp
@@ -8,8 +8,6 @@ int minesweeperPlayer(char myGrid[][25], char validGrid[][25], int, int, int);
 int checkValid(int, int, int);
 void printGrid(char myGrid[][25], int);
 int checkMine(char realGrid[][25], int, int);
-int inputValid(int);
-int test(int argc, char **argv);
 int countAdjacentMines(char realGrid[][25], int, int, int);
 void generateMinesweeper(char realGrid[][25], char myGrid[][25], int, int);
 int unknownsLeft(char myGrid[][25], int);
