5b6ecebe454f93881d29dbcc6a65010ca81eb45a
Sakshi <tiwari.sakshi96.st@gmail.com>
2018-02-05 19:01:01 +0530

    Added lib

---
+++ cmakeVersion/lib/googletest-master/googlemock/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/sakshi/Minesweeper1/lib/googletest-master/googletest
+# Build directory: /home/sakshi/Minesweeper1/lib/googletest-master/googlemock/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
